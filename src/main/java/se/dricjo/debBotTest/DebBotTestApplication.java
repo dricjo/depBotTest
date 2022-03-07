6784b0679a17bfccaaff8e880464e1e14b54546d
rickard <rickard.johansson@deltaprojects.com>
2022-03-07 15:12:30 +0100

    fun

---
+++ src/main/java/se/dricjo/debBotTest/DebBotTestApplication.java
@@ -0,0 +1,13 @@
+package se.dricjo.debBotTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebBotTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebBotTestApplication.class, args);
+	}
+
+}
